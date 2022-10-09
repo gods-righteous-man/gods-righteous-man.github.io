9fa139a49e13dfea3fa095bf8b71667cdce67116
Gaurav Makasare <makasare@usc.edu>
2022-10-09 16:09:47 -0700

    changes

--- src/helper/ProjectList.js
+++ src/helper/ProjectList.js
@@ -5,6 +5,7 @@ import Proj4 from '../assets/proj4.png';
 import Proj5 from '../assets/proj5.png';
 import Proj6 from '../assets/proj6.png';
 
+
 export const projectlist = [
     {
         name: "Team Generator Application",
