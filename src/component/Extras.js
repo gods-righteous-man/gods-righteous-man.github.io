fa50cd1b14c9573d8339a2c839dea4f15b9b66cb
Gaurav Makasare <makasare@usc.edu>
2023-04-03 21:19:34 -0700

    added certificates

--- src/component/Extras.js
+++ src/component/Extras.js
@@ -5,6 +5,8 @@ import DV from "../assets/dv.pdf";
 import BC from "../assets/bc.pdf";
 import Transcript from "../assets/Transcripts.pdf";
 import '../styles/Extras.css';
+import AWS from "../assets/aws.pdf";
+import Udemy from "../assets/udemy.pdf"
 
 
 function Extras() {
@@ -13,10 +15,12 @@ function Extras() {
       <div className='subdiv'>
         <ul>
         <li><a href = {Resume} target = "_blank" rel="noreferrer">View Resume</a></li>
+        <li><a href = {AWS} target = "_blank" rel="noreferrer">AWS Cloud Quest: Cloud Practitioner Certificate</a></li>
         <li><a href = {Transcript} target = "_blank" rel="noreferrer">View Unofficial Transcript</a></li>
-        <li><a href = {BC} target = "_blank" rel="noreferrer">Coursera Blockchain Certificate</a></li>
-        <li><a href = {DV} target = "_blank" rel="noreferrer">Coursera Datavisualizations Certificate</a></li>
-        <li><a href = {TC} target = "_blank" rel="noreferrer">Coursera Tensorflow with Sagemaker Certificate</a></li>
+        <li><a href = {BC} target = "_blank" rel="noreferrer">Coursera: Blockchain Certificate</a></li>
+        <li><a href = {Udemy} target = "_blank" rel="noreferrer">Udemy: The Complete Cyber Security Course: Network Security</a></li>
+        <li><a href = {DV} target = "_blank" rel="noreferrer">Coursera: Datavisualizations Certificate</a></li>
+        <li><a href = {TC} target = "_blank" rel="noreferrer">Coursera: Tensorflow with Sagemaker Certificate</a></li>
         </ul>
       </div>
         
